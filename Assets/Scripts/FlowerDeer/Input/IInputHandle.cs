fc670d2f1345c5b58b559801c032359558fff1ec
Coremos <coremos@naver.com>
2023-04-01 08:28:37 +0900

    Add: 입력 관련 클래스 추가
    IInputHandle.cs
    - 입력을 위임받을 수 있도록 만들어진 인터페이스
    
    InputHandler.cs
    - 입력을 판정하고 등록된 IInputHandle 객체에 입력을 위임

---
+++ Assets/Scripts/FlowerDeer/Input/IInputHandle.cs
@@ -0,0 +1,9 @@
+﻿namespace FlowerDeer.InputSystem
+{
+    public interface IInputHandle
+    {
+        void OnInputKey(KeyType keyType);
+        void OnInputKeyDown(KeyType keyType);
+        void OnInputKeyUp(KeyType keyType);
+    }
+}
---
+++ Assets/Scripts/FlowerDeer/Input/InputHandler.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace FlowerDeer.InputSystem
+{
+    public enum KeyType { LEFT, RIGHT, JUMP };
+
+    public class InputHandler : MonoBehaviour
+    {
+        private Dictionary<KeyType, KeyCode> keys;
+        private KeyValuePair<KeyType, KeyCode>[] keyPairs;
+        private int keyLength;
+        private static List<IInputHandle> inputHandles = new List<IInputHandle>();
+
+        public static void RegisterInputHandle(IInputHandle inputHandle)
+        {
+            inputHandles.Add(inputHandle);
+        }
+
+        private void Awake()
+        {
+            InitializeKey();
+            UpdateKeyPair();
+        }
+
+        private void InitializeKey()
+        {
+            keys = new Dictionary<KeyType, KeyCode>();
+
+            keys.Add(KeyType.LEFT, KeyCode.LeftArrow);
+            keys.Add(KeyType.RIGHT, KeyCode.RightArrow);
+        }
+
+        private void UpdateKeyPair()
+        {
+            keyPairs = keys.ToArray();
+            keyLength = keyPairs.Length;
+        }
+
+        private void Update()
+        {
+            for (int index = 0; index < keyLength; index++)
+            {
+                if (Input.GetKey(keyPairs[index].Value))
+                {
+                    for (int handleIndex = 0; handleIndex < inputHandles.Count; handleIndex++)
+                    {
+                        inputHandles[handleIndex].OnInputKey(keyPairs[index].Key);
+                    }
+                }
+            }
+        }
+    }
+}
