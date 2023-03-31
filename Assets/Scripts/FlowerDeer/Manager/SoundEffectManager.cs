796e04d3892e74c35f39a6414ba926514e1546b9
Coremos <coremos@naver.com>
2023-04-01 08:34:58 +0900

    Add: 매니저 클래스 추가
    SoundEffectManager.cs
    - 효과음을 재생할 수 있도록 추가
    
    TimerManager.cs
    - 지정된 시간이 지난 후 메서드를 호춣할 수 있도록 추가

---
+++ Assets/Scripts/FlowerDeer/Manager/SoundEffectManager.cs
@@ -0,0 +1,49 @@
+﻿using FlowerDeer.Utility;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FlowerDeer.Manager
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class SoundEffectManager : Singleton<SoundEffectManager>
+    {
+        [SerializeField] private AudioSource audioSource;
+        [SerializeField] private AudioClip[] clips;
+
+        private Dictionary<string, AudioClip> clipDictionary;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            InitializeAudioSourceComponent();
+            InitializeClipDictionary();
+        }
+
+        private void InitializeAudioSourceComponent()
+        {
+            if (audioSource == null)
+            {
+                TryGetComponent(out audioSource);
+            }
+        }
+
+        private void InitializeClipDictionary()
+        {
+            clipDictionary = new Dictionary<string, AudioClip>();
+
+            for (int index = 0; index < clips.Length; index++)
+            {
+                clipDictionary.Add(clips[index].name, clips[index]);
+            }
+        }
+
+        public void PlaySound(string name, float volume = 1.0f, float pitch = 1.0f)
+        {
+            if (!clipDictionary.ContainsKey(name)) return;
+
+            audioSource.pitch = pitch;
+            audioSource.PlayOneShot(clipDictionary[name], volume);
+        }
+    }
+}
---
+++ Assets/Scripts/FlowerDeer/Manager/TimerManager.cs
@@ -0,0 +1,26 @@
+﻿using FlowerDeer.Utility;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace FlowerDeer.Manager
+{
+    public class TimerManager : Singleton<TimerManager>
+    {
+        public Coroutine StartTimer(float time, Action onCompleteEvent)
+        {
+            return StartCoroutine(Timer(time, onCompleteEvent));
+        }
+
+        private IEnumerator Timer(float time, Action onCompleteEvent)
+        {
+            while (time > 0)
+            {
+                time -= Time.deltaTime;
+                yield return null;
+            }
+            onCompleteEvent?.Invoke();
+            yield return null;
+        }
+    }
+}
